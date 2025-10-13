419cc5caf0446567f77bc72edfcbed40afa3d7f5
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-10-13 15:06:08 -0300

    exibindo dados do Json

--- api_local.php
+++ api_local.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "Ola!";
+$url = "dados.json";
+$resposta = file_get_contents($url);
+$dados = json_decode($resposta, true);
+
+echo $dados["nome"];
+echo "<br><hr>";
+echo $dados["hobbies"][0];
+echo "<br><hr>";
+echo $dados["gatos"]["nomes"][0];
 
 ?>
