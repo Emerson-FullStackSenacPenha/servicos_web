ad83bf6b517b42fba60eff8bf6bd57f0e3dad682
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-10-13 16:13:15 -0300

    acessando api externa

--- resgateDDD.php
+++ resgateDDD.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "Ola Suavesssssss";
+$url = "https://brasilapi.com.br/api/ddd/v1/11";
+$resposta = file_get_contents($url);
+$dados = json_decode($resposta, true);
+
+$estado = $dados['state'];
+$cidade = $dados['cities'][0];
+
+echo "<h3> Estado: $estado </h3>";
+echo "<h4> Cidade: $cidade </h4>";
 
 ?>
