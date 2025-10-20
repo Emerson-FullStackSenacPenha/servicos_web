e77e4af2c36622a88f03e25ff7ce36481973ff25
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-10-20 17:02:23 -0300

    criação e utilização da api e do json

--- api.php
+++ api.php
@@ -6,15 +6,59 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 
-$alunos = [
+// Sistemas de serviço web
 
-    ["nome" => "Anna", "curso" => "Design", "idade" => 19],
-    ["nome" => "Carlos", "curso" => "TI", "idade" => 22],
-    ["nome" => "Karla", "curso" => "Games", "idade" => 25]
+$pacocas = json_decode( file_get_contents("pacoca.json"), true );
 
-];
+// variavel para guardar o que tem na posição enviada pelo link
+$pacoca_especifica = $_GET['pacocas'];
+
+
+
+switch($pacoca_especifica){
+
+    case "coco":
+
+        $pacoca_coco = $pacocas['pacocas']['Paçoca de coco'];
+
+        break;
+
+    default:
+
+        echo json_encode($pacocas);
+        
+        break;
+
+}
+
+function cadastrar_pacoca($nome, $tipo, $origem, $nutrientes){
+
+    $pacocas['paçocas']['$nome']['nome'] = "$nome";
+    $pacocas['paçocas']['$nome']['Tipo'] = "$tipo";
+    $pacocas['paçocas']['$nome']['Origem'] = "$origem";
+    $pacocas['paçocas']['$nome']['Nutrientes'] = "$nutrientes";
+
+    if(false){
+        salvar_dados($pacocas);
+    }
+
+}
+
+function salvar_dados($variavel){
+
+    //file_put Salvar arquivo   e   json_encode converte
+    file_put_contents('pacoca.json', json_encode($variavel, JSON_PRETTY_PRINT));
+
+}
+
+
+// echo $pacocas['paçocas']['Paçoca de coco']['nome'];
+// echo "<br>";
+// echo $pacocas['paçocas']['Paçoca de Mel']['nome'];
+
+
 
 // Saída da API
-echo json_encode($alunos);
+echo json_encode($pacoca_especifica);
 
 ?>
--- cliente.php
+++ cliente.php
@@ -2,21 +2,19 @@
 
 /* Consumo de API - Utiliza de um serviço WEB */
 
-    // Requisição através de uma URL (Protocolo HTTP)
-    $url = "http://localhost/servicos_web/api.php";
-
-
+    // Requisição GET (Protocolo HTTP)
+    $url = "http://localhost/servicos_web/api.php?pacocas=coco";
 
     // Guardar um valor de resposta da API
     $resposta = file_get_contents($url);
 
     // Converter o Json em array asociativo
-    $dados = json_decode($resposta, true);
+    // $dados = json_decode($resposta, true);
 
     // Pegar o valor do array
-    $valor = $dados[""];
+    // $valor = $dados[""];
 
     // Mostrar no navegador
-    echo $valor;
+    echo $resposta;
 
 ?>
