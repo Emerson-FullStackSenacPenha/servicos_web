702d25eb0a86d69119b70b806ed0b5578c3376da
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-10-23 16:11:05 -0300

    criação de api

--- exercicio_4/api.php
+++ exercicio_4/api.php
@@ -5,15 +5,134 @@ header("Access-Control-Allow-Origin: *");
 
 $paes = json_decode(file_get_contents("pao.json"), true);
 
-$pao_especifico = $_GET['pães'];
+$pao_especifico = $_GET['paes'];
 
 switch($pao_especifico){
 
+    // Frances
+    case "frances":
+
+        $pao = $paes['pães']['Pão frances'];
+        echo json_encode($pao);
+        break;
+
+    case "frances-nome":
+
+        $pao = $paes['pães']['Pão frances']['nome'];
+        echo json_encode($pao);
+        break;
+
+    case "frances-tipo":
+
+        $pao = $paes['pães']['Pão frances']['Tipo'];
+        echo json_encode($pao);
+        break;
+
+    case "frances-origem":
+
+        $pao = $paes['pães']['Pão frances']['Origem'];
+        echo json_encode($pao);
+        break;
+
+    case "frances-nutri":
+
+        $pao = $paes['pães']['Pão frances']['Nutrientes'];
+        echo json_encode($pao);
+        break;
+    
+    // Forma
+    case "forma":
+
+        $pao = $paes['pães']['Pão forma'];
+        echo json_encode($pao);
+        break;
+
+    case "forma-nome":
+
+        $pao = $paes['pães']['Pão forma']['nome'];
+        echo json_encode($pao);
+        break;
+
+    case "forma-tipo":
+
+        $pao = $paes['pães']['Pão forma']['Tipo'];
+        echo json_encode($pao);
+        break;
+
+    case "forma-origem":
+
+        $pao = $paes['pães']['Pão forma']['Origem'];
+        echo json_encode($pao);
+        break;
+
+    case "forma-nutri":
+
+        $pao = $paes['pães']['Pão forma']['Nutrientes'];
+        echo json_encode($pao);
+        break;
+    
+    // Centeio
+    case "centeio":
+
+        $pao = $paes['pães']['Pão centeio'];
+        echo json_encode($pao);
+        break;
+
+    case "centeio-nome":
+
+        $pao = $paes['pães']['Pão centeio']['nome'];
+        echo json_encode($pao);
+        break;
+
+    case "centeio-tipo":
+
+        $pao = $paes['pães']['Pão centeio']['Tipo'];
+        echo json_encode($pao);
+        break;
+
+    case "centeio-origem":
+
+        $pao = $paes['pães']['Pão centeio']['Origem'];
+        echo json_encode($pao);
+        break;
+
+    case "centeio-nutri":
+
+        $pao = $paes['pães']['Pão centeio']['Nutrientes'];
+        echo json_encode($pao);
+        break;    
+
+    // doce
     case "doce":
 
-        $pao_doce = $paes['pães']['Pão doce'];
-        echo json_encode($pao_doce);
+        $pao = $paes['pães']['Pão doce'];
+        echo json_encode($pao);
         break;
+
+    case "doce-nome":
+
+        $pao = $paes['pães']['Pão doce']['nome'];
+        echo json_encode($pao);
+        break;
+
+    case "doce-tipo":
+
+        $pao = $paes['pães']['Pão doce']['Tipo'];
+        echo json_encode($pao);
+        break;
+
+    case "doce-origem":
+
+        $pao = $paes['pães']['Pão doce']['Origem'];
+        echo json_encode($pao);
+        break;
+
+    case "doce-nutri":
+
+        $pao = $paes['pães']['Pão doce']['Nutrientes'];
+        echo json_encode($pao);
+        break;  
+    
 
     default:
 
--- exercicio_4/cliente.php
+++ exercicio_4/cliente.php
@@ -1,7 +1,7 @@
 <?php
 
-$url = "http://localhost/servicos_web/exercicio_4/api.php?pães=doce";
+$url = "http://localhost/servicos_web/exercicio_4/api.php?paes=doce";
 $resposta = file_get_contents($url);
 echo $resposta;
-
+$valores = json_decode($resposta, true);
 ?>
