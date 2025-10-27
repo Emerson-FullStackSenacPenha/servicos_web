0dc834a328ddb13a2bb7429330d24f8e31835638
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-10-27 17:11:26 -0300

    Documentação de Api pao

--- exercicio_4/cliente.php
+++ exercicio_4/cliente.php
@@ -1,8 +1,12 @@
 <?php
 
-$url = "http://localhost/servicos_web/exercicio_4/api.php?paes=doce";
+$url = "http://localhost/servicos_web/exercicio_4/api.php";
 $resposta = file_get_contents($url);
 $valores = json_decode($resposta, true);
 echo $resposta;
+echo "<br>";
+echo "<br>";
+// Como acessar os dados
+echo $valores['pães']['Pão doce']['nome'];
 
 ?>
